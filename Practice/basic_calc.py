08a3115ab24c9fc58b293e1e80ce2ae1b2e27acb
Deepuk555 <71319706+Deepuk555@users.noreply.github.com>
2022-04-07 15:18:07 +0530

    Create basic_calc.py

---
+++ Practice/basic_calc.py
@@ -0,0 +1,31 @@
+def calcu():
+    
+    num = 0
+    
+    while num != 6:
+        
+        print("enter two numbers: ")
+        num1 = int(input())
+        num2 = int(input())
+        print("1. addition")
+        print("2. subtraction")
+        print("3. multiplication")
+        print("4. division")
+        print("5. remainder")
+        print("6. exit")
+        
+        print("enter choice")
+        num = int(input())
+        if num == 1:
+            print(num1 + num2)
+        elif num == 2:
+            print(num1 + num2)
+        elif num == 3:
+            print(num1 + num2)
+        elif num == 4:
+            print(num1 + num2)
+        elif num == 5:
+            print(num1 + num2)
+            
+    
+calcu()
